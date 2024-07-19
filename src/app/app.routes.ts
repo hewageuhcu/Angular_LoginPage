bc13962d73154d12450df3200a3eb4ae5715bfe1
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-07-20 04:27:31 +0900

    Created a login page using Angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+
+
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch: 'full'
+    },
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    
+];
